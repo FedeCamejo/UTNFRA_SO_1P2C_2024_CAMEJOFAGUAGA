beb79500b6229c26172a456a83e3269c81b90ac8
FedeCamejo <fede-camejo@hotmail.com>
2024-10-03 04:25:28 +0000

    ADD: armado del punto 1

--- RTA_SCRIPT_Examen_20241002/Punto_A.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_A.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creo los grupos de alumnos, parciales y profesores"
+
 mkdir -p $HOME/punto_A/Examenes-UTN/alumno_{1..3}/parcial_{1..3}/ $HOME/punto_A/Examenes-UTN/profesores
 
 

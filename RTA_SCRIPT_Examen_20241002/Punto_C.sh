78c529b919787b4b86c688c9ff70c468379dccf6
FedeCamejo <fede-camejo@hotmail.com>
2024-10-06 00:08:40 +0000

    ADD3:creacion del punto 3

---
+++ RTA_SCRIPT_Examen_20241002/Punto_C.sh
@@ -0,0 +1,36 @@
+#! /bin/bash 
+
+
+echo "Agrego los usuarios" 
+ sudo useradd p1c2_2024_A1
+ sudo useradd p1c2_2024_A2
+ sudo useradd p1c2_2024_A3
+ sudo useradd p1c2_2024_P1
+
+echo "Creo los grupos"
+ sudo groupadd p1c2_2024_gAlumno
+ sudo groupadd p1c2_2024_gProfesores
+
+echo "añado a los usuarios a sus grupos" 
+ sudo usermod -aG  p1c2_2024_gAlumno p1c2_2024_A1
+ sudo usermod -aG  p1c2_2024_gAlumno p1c2_2024_A2
+ sudo usermod -aG  p1c2_2024_gAlumno p1c2_2024_A3
+ sudo usermod -aG  p1c2_2024_gProfesores p1c2_2024_P1
+
+echo "defino dueños de grupo"
+sudo chown p1c2_2024_A1:p1c2_2024_gAlumno /Examenes-UTN/alumno_1
+sudo chown p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
+sudo chown p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
+sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+
+echo "doy permisos a cada uno"
+sudo chmod 750 /Examenes-UTN/alumno_2
+sudo chmod 750 /Examenes-UTN/alumno_1
+sudo chmod 775 /Examenes-UTN/profesores
+sudo chmod 700 /Examenes-UTN/alumno_3
+
+echo "redirigo todo a validar.txt"
+sudo -u p1c2_2024_A1 sh -c 'whoami > /Examenes-UTN/alumno_1/validar.txt'
+sudo -u p1c2_2024_A2 sh -c 'whoami > /Examenes-UTN/alumno_2/validar.txt'
+sudo -u p1c2_2024_A3 sh -c 'whoami > /Examenes-UTN/alumno_3/validar.txt'
+

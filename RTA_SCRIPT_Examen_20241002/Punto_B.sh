ccd52317b4e3e564b4e04360359e01394554f124
FedeCamejo <fede-camejo@hotmail.com>
2024-10-06 23:51:04 -0300

    Update Punto_B.sh

--- RTA_SCRIPT_Examen_20241002/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241002/Punto_B.sh
@@ -3,28 +3,23 @@
 echo "Creando una partición extendida en el disco /dev/sdc..."
 
 # Crear partición extendida y lógicas usando fdisk
-{
-    echo "o"    # Crear una nueva tabla de particiones
-    echo "n"    # Nueva partición
-    echo "p"    # Primaria
-    echo "1"    # Número de partición
-    echo ""     # Primer sector (por defecto)
-    echo "+10G" # Tamaño 10 GB (esto creará la extendida)
+sudo fdisk /dev/sdc/
 
-    echo "n"    # Nueva partición
-    echo "e"    # Extendida
-    echo "2"    # Número de partición
-    echo ""     # Primer sector (por defecto)
-    echo ""     # Usar todo el espacio restante
+{
+  echo  "p" # Ver la tabla de particiones
+   echo  "n" # Crear una nueva partición
+    echo "e" # Partición extendida
+    echo "" # Aceptar el sector de inicio por defecto
+    echo "+10G" # Tamaño de la partición extendida, le entrego todo el disco
 
     # Crear particiones lógicas de 1 GB
-    for i in {5..14}
+    for i in {1..10}
     do
         echo "n"  # Nueva partición
         echo "l"  # Lógica
         echo "$i" # Número de partición
         echo ""   # Primer sector (por defecto)
-        if [ "$i" -eq 14 ]; then
+        if [ "$i" -eq 10 ]; then
             echo "" # Usar el espacio restante en la última partición
         else
             echo "+1G" # Tamaño 1 GB para las demás
@@ -35,13 +30,13 @@ echo "Creando una partición extendida en el disco /dev/sdc..."
 } | sudo fdisk /dev/sdc
 
 echo "Formateando las particiones lógicas..."
-for i in {5..14}
+for i in {1..10}
 do
     sudo mkfs.ext4 /dev/sdc$i
 done
 
 echo "Montando las particiones..."
-for i in {5..14}
+for i in {1..10}
 do
     sudo mount /dev/sdc$i /punto_A/Examenes-UTN/alumno_$(( (i-5)/3 + 2 ))/parcial_$(( (i-5)%3 + 1 ))
 done
